c247e13c96b4f777c52ce9996faa2adb82c8e2c2
Fabio Ramos <fabiowilliam466@gmail.com>
2025-06-18 16:30:13 -0400

    Atualiza a função de login para usar credenciais do arquivo JSON e altera a versão do Cypress para 14.5.0

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,8 +1,14 @@
 // cypress/support/commands.ts
 
-Cypress.Commands.add('typelogin', (url, username, password) => {
-  cy.visit(url);
-  cy.get('#login').type(username);
-  cy.get('#senha').type(password);
-  cy.get('.css-cioejf-LoginForm > .MuiButton-root').click(); //Botão Acessar da página principal
+Cypress.Commands.add('typelogin', () => {
+  // Carregar o arquivo JSON de credenciais
+  cy.fixture('credenciais_sistema.json').then((credenciais) => {
+    const { login, senha } = credenciais.credenciais[0]; // Usando o primeiro conjunto de credenciais (grupo1_gestor@sig.com)
+
+    // Acessar o sistema com a URL e as credenciais
+    cy.visit(credenciais.url_sistema);
+    cy.get('#login').type(login);
+    cy.get('#senha').type(senha);
+    cy.get('.css-1wz47u4 > .MuiButton-root').click(); // Botão Acessar da página de login
+  });
 });
